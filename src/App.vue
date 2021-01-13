ac409ca3cebd81c4dc338d5f4d3d29a94295056e
xiaobazai <2840949881@qq.com>
2021-01-13 13:14:05 +0800

    更改了下

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
 }
 
